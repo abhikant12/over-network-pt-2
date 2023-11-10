a11cc78a98e5cad77c911251f585a9c3ba9638b5
Abhikant Kumar <89623613+abhikant12@users.noreply.github.com>
2023-11-10 09:48:26 +0530

    completed

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,3 +1,4 @@
+// RootLayout.jsx
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Accounts from '@/components/Accounts';
@@ -13,7 +14,7 @@ import LoginWindow from './loginWindow';
 const inter = Inter({ subsets: ['latin'] });
 
 const cal = localFont({
-  src: './cal.woff2',
+  src: '/cal.woff2', // Make sure the font file is accessible at this path
   display: 'swap',
   variable: '--font-cal',
 });
@@ -21,17 +22,17 @@ const cal = localFont({
 const matter = localFont({
   src: [
     {
-      path: './Matter-Regular.woff2',
+      path: '/Matter-Regular.woff2', // Make sure the font file is accessible at this path
       weight: '400',
       style: 'normal',
     },
     {
-      path: './Matter-Medium.woff2',
+      path: '/Matter-Medium.woff2', // Make sure the font file is accessible at this path
       weight: '400',
       style: 'normal',
     },
     {
-      path: './Matter-Bold.woff2',
+      path: '/Matter-Bold.woff2', // Make sure the font file is accessible at this path
       weight: '700',
       style: 'normal',
     },
@@ -47,60 +48,41 @@ export default async function RootLayout({
 }) {
   const me = await getMe();
 
-  /*
-    This function is called to set up the profile of a new user. It is called
-    when a user submits the login form. It takes a FormData object as an argument,
-    which contains the username and name of the new user.
-    @param form - FormData object containing the username and name of the new user
-  */
   const setUpProfile = async (form: FormData) => {
-    /*
-      TODO #1: Indicate that this function is a server function by adding 'use server';
-    */
+    server;
 
-    /*
-      TODO #2: Create the new User object with a username, name, and privateKey
-    
-      HINT: 
-        - 
-        - Use the newPrivateKey() function to generate a new private key for the user
-    */
+    const privateKey = newPrivateKey();
+    const user = {
+      username: form.get('username') as string,
+      name: form.get('name') as string,
+      privateKey,
+    };
 
-    /* 
-      TODO #3: Store the user in the local account cache
+    storeUser(user);
 
-      HINT: Use the storeUser() function to store the user
-    */
-
-    /* 
-      TODO #4: Set up a try catch block to create the user's profile and log them in if successful.
-
-      HINT: 
-        - Use the createProfile() and login() functions to create the user's 
-          profile and log them in
-        
-        - In the catch block, use the dropUser() function to remove the user 
-          from the local account cache. Then, throw the error to be caught by the catch block in
-          the loginWindow.tsx file.
-    */
-  }
+    try {
+      await createProfile(user);
+      await login(user.username, user.privateKey);
+    } catch (error) {
+      dropUser(user);
+      throw error;
+    }
+  };
 
   if (!me) {
     return (
       <html lang='en'>
-        <body
-          className={`${cal.variable} ${matter.variable} ${inter.className}`}
-        >
+        <body className={`${cal.variable} ${matter.variable} ${inter.className}`}>
           <div className='absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center'>
             <div className='w-full max-w-xs space-y-6 rounded-xl border border-neutral-300 bg-neutral-400 px-6 py-4'>
               <p className='text-2xl font-bold'>Log in to Over Network</p>
-              {
-                await getNumberOfUsers() >= 2 ? 
+              {getNumberOfUsers() >= 2 ? (
                 <p className='text-sm font-medium text-neutral-100'>
                   You have reached the maximum number of accounts.
-                </p> :
-                <LoginWindow setUpProfile={setUpProfile} /> 
-              }
+                </p>
+              ) : (
+                <LoginWindow setUpProfile={setUpProfile} />
+              )}
               <Accounts />
             </div>
           </div>
@@ -114,18 +96,14 @@ export default async function RootLayout({
       <body className={`${cal.variable} ${matter.variable} ${inter.className}`}>
         <div className='flex h-screen flex-row items-center justify-center'>
           <form action={logout} className='flex flex-col gap-3 border rounded-xl px-4 py-2 bg-neutral-400 border-neutral-300'>
-            <p className='text-2xl font-bold'>
-              You are logged in!
-            </p>
+            <p className='text-2xl font-bold'>You are logged in!</p>
             <p className='text-xs font-bold uppercase text-neutral-100'>
               Your account
             </p>
             <div className='flex flex-row items-center justify-start space-x-3 rounded-xl border border-neutral-200 bg-neutral-300 px-4 py-2'>
               <Avatar className='h-11 w-11' user={me} />
               <div className='flex flex-col items-start -space-y-0.5'>
-                <p className='text-lg font-medium capitalize'>
-                  {me.name}
-                </p>
+                <p className='text-lg font-medium capitalize'>{me.name}</p>
                 <p className='text-neutral-100'>@{me.username}</p>
               </div>
             </div>
